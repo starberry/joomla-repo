d8db6907fb8d74596cfff0b62a9d1b85e5e02322
Tom Gidden <gid@starberry.tv>
2013-12-24 12:04:42 +0000

    Import of Joomla 3.2.1

--- httpdocs/tests/system/webdriver/tests/content/CategoryManager0003Test.php
+++ httpdocs/tests/system/webdriver/tests/content/CategoryManager0003Test.php
@@ -60,16 +60,15 @@ class CategoryManager0003Test extends JoomlaWebdriverTestCase
 	 */
 	public function getAllInputFields_ScreenDisplayed_EqualExpected()
 	{
-		$this->categoryManagerPage = $this->getPageObject('CategoryManagerPage');
 		$this->categoryManagerPage->clickButton('toolbar-new');
 		$categoryEditPage = $this->getPageObject('CategoryEditPage');
-		$testElements = $categoryEditPage->getAllInputFields(array('general', 'publishing', 'options', 'metadata','rules'));
-		$actualFields = array();
-		foreach ($testElements as $el)
-		{
-			$el->labelText = (substr($el->labelText, -2) == ' *') ? substr($el->labelText, 0, -2) : $el->labelText;
-			$actualFields[] = array('label' => $el->labelText, 'id' => $el->id, 'type' => $el->tag, 'tab' => $el->tab);
-		}
+
+		// Option to print actual element array
+		/* @var $categoryEditPage CategoryEditPage */
+// 	 	$categoryEditPage->printFieldArray($categoryEditPage->getAllInputFields($categoryEditPage->tabs));
+
+		$testElements = $categoryEditPage->getAllInputFields($categoryEditPage->tabs);
+		$actualFields = $this->getActualFieldsFromElements($testElements);
 		$this->assertEquals($categoryEditPage->inputFields, $actualFields);
 		$categoryEditPage->clickButton('toolbar-cancel');
 		$this->categoryManagerPage = $this->getPageObject('CategoryManagerPage');
@@ -111,12 +110,11 @@ class CategoryManager0003Test extends JoomlaWebdriverTestCase
 	{
 		$salt = rand();
 		$categoryName = 'ABC' . $salt;
-		$alias = 'ABC_Alias'. $salt;
-		$expected_alias='abc-alias'.$salt;
-		$desc = 'ABC_Desc';
+		$expected_alias='abc-alias' . $salt;
+		$desc = $categoryName . ' Description';
 		$this->categoryManagerPage = $this->getPageObject('CategoryManagerPage');
 		$this->assertFalse($this->categoryManagerPage->getRowNumber($categoryName), 'Test Category should not be present');
-		$this->categoryManagerPage->addCategory($categoryName,$alias,$desc);
+		$this->categoryManagerPage->addCategory($categoryName, $desc, array('Alias' => $expected_alias));
 		$message = $this->categoryManagerPage->getAlertMessage();
 		$this->assertTrue(strpos($message, 'Category successfully saved') >= 0, 'Category save should return success');
 		$values = $this->categoryManagerPage->getFieldValues('CategoryEditPage', $categoryName, array('Title', 'Alias'));
@@ -132,8 +130,7 @@ class CategoryManager0003Test extends JoomlaWebdriverTestCase
 	{
 		$salt = rand();
 		$categoryName = 'ABC' . $salt;
-		$alias = 'ABC_Alias'. $salt;
-		$expected_alias='abc-alias'.$salt;
+		$alias='abc-alias' . $salt;
 		$desc = 'ABC_Description';
 		$expected_desc='<p>ABC_Description</p>';
 		$this->categoryManagerPage = $this->getPageObject('CategoryManagerPage');
@@ -141,8 +138,9 @@ class CategoryManager0003Test extends JoomlaWebdriverTestCase
 		$this->categoryManagerPage->addCategory($categoryName);
 		$this->categoryManagerPage->editCategory($categoryName, array('Alias' => $alias, 'Description' => $desc));
 		$values = $this->categoryManagerPage->getFieldValues('CategoryEditPage', $categoryName, array('Alias', 'Description'));
-		$this->assertEquals(array($expected_alias, $expected_desc), $values, 'Actual values should match expected');
+		$this->assertEquals(array($alias, $expected_desc), $values, 'Actual values should match expected');
 		$this->categoryManagerPage->trashAndDelete($categoryName);
+		$this->assertFalse($this->categoryManagerPage->getRowNumber($categoryName), 'Test category should not be present');
 	}
 
 	/**
@@ -151,14 +149,16 @@ class CategoryManager0003Test extends JoomlaWebdriverTestCase
 	public function changeCategoryState_ChangeEnabledUsingToolbar_EnabledChanged()
 	{
 		$salt = rand();
+		$categoryName = 'Test Category ' . $salt;
 		$this->categoryManagerPage = $this->getPageObject('CategoryManagerPage');
-		$this->categoryManagerPage->addCategory('ABC_Test', $salt);
-		$state = $this->categoryManagerPage->getState('ABC_Test');
+		$this->categoryManagerPage->addCategory($categoryName);
+		$state = $this->categoryManagerPage->getState($categoryName);
 		$this->assertEquals('published', $state, 'Initial state should be published');
-		$this->categoryManagerPage->changeCategoryState('ABC_Test', 'unpublished');
-		$state = $this->categoryManagerPage->getState('ABC_Test');
+		$this->categoryManagerPage->changeCategoryState($categoryName, 'unpublished');
+		$state = $this->categoryManagerPage->getState($categoryName);
 		$this->assertEquals('unpublished', $state, 'State should be unpublished');
-		$this->categoryManagerPage->trashAndDelete('ABC_Test');
+		$this->categoryManagerPage->trashAndDelete($categoryName);
+		$this->assertFalse($this->categoryManagerPage->getRowNumber($categoryName), 'Test category should not be present');
 	}
 
 
